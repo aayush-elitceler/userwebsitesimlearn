afe99f297c4148ab9641d1935944cb17e9daa2ad
samitelitceler <samit@elitceler.com>
2025-07-19 17:24:56 +0530

    change

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,22 @@
 // middleware.ts
 import { NextRequest, NextResponse } from 'next/server';
+import jwt from 'jsonwebtoken';
 
 export function middleware(req: NextRequest) {
   const token = req.cookies.get('token')?.value;
+
   if (!token) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
 
-  // Decode payload (NOT secure, just for reading claims)
   try {
-    // Optionally check exp, roles, etc.
+    jwt.verify(token, process.env.JWT_SECRET!);
     return NextResponse.next();
-  } catch (e) {
+  } catch (error) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
 }
 
 export const config = {
-  matcher: ['/dashboard'],
+  matcher: ['/home'],
 };
