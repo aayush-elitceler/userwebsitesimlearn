dfa7c97c10b59fe1331dccd61abaf49cebbb07f2
samitelitceler <samit@elitceler.com>
2025-07-31 17:29:58 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,10 @@
-
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    images: {
-        domains: ['reportal-media.s3.ap-south-1.amazonaws.com'],
-    },
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'Permissions-Policy',
-                        value: 'microphone=*',
-                    },
-                    {
-                        key: 'Feature-Policy',
-                        value: 'microphone *',
-                    },
-                ],
-            },
-        ];
-    },
+  output: 'standalone',
+  images: {
+    domains: ['reportal-media.s3.ap-south-1.amazonaws.com'],
+  },
 };
 
 export default nextConfig;
